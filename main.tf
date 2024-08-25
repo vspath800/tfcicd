cc833b6a92a5e4db97687d1f50c0b2487adbc525
vspath800 <krackermal441@gmail.com>
2024-08-25 11:25:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "google_storage_bucket" "auto-expire" {
+  name          = "my-storage-bucket-250820241125"   # Ensure this is globally unique
+  location      = "US"                        # Bucket location
+  force_destroy = true                        # Allows deleting the bucket even if it contains objects
+  project       = "gcp-class-1-427416"        # Your Google Cloud project ID
+  public_access_prevention = "enforced"        # Prevents public access
+
+  # Optional: Additional settings can be added here
+  # e.g., versioning, lifecycle rules, etc.
+}
