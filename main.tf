2fde3cc4c7abb24e5f225ecb17962c4408001e56
vspath800 <krackermal441@gmail.com>
2024-08-26 06:23:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "auto-expire" {
   name          = "my-storage-bucket-250820241125"   # Ensure this is globally unique
-  location      = "australia-southeast2"                        # Bucket location
+  location      = "us-west2"                        # Bucket location
   force_destroy = true                        # Allows deleting the bucket even if it contains objects
   project       = "gcp-class-1-427416"        # Your Google Cloud project ID
   public_access_prevention = "enforced"        # Prevents public access
